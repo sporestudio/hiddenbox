3a1157255397f15f439865b0becceb21741ab0f1
sporestudio <jrodcas@proton.me>
2025-03-22 19:19:36 +0100

    chore: Add necessary vars for instance deployment
    Add variables for instance deployment as the instances' ami, number of
    instances, user...

--- variables.tf
+++ variables.tf
@@ -21,3 +21,27 @@ variable "region" {
   type = string
   default = "us-west-2"
 }
+
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type = string
+  default = "ami-0c55b159cbfafe1f0"
+}
+
+variable "user" {
+  description = "The user data to provide when launching the instance"
+  type = string
+  default = "provisioner"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance to launch"
+  type = string
+  default = "t3.micro"
+}
+
+variable "instances_amount" {
+  description = "The amount of instances to launch"
+  type = number
+  default = 3
+}
