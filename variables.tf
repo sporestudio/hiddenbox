d0aa5520c2c763a5690740c16f2db4831d252ded
sporestudio <jrodcas@proton.me>
2025-03-23 18:24:37 +0100

    chore: add new vars in variables.tf
    Add new vars for the project:
    - Disk size for EBS volumes.
    - Split the main instance from storage instances nodes.
    - Fix public key path.
    - New AMI for ubuntu instances.

--- variables.tf
+++ variables.tf
@@ -19,13 +19,13 @@ variable "aws_session_token" {
 variable "region" {
   description = "AWS region"
   type = string
-  default = "us-west-2"
+  default = "us-east-1"
 }
 
-variable "ami" {
-  description = "The AMI to use for the instance"
-  type = string
-  default = "ami-0c55b159cbfafe1f0"
+variable "ubuntu_ami" {
+  description = "Ubuntu 22.04 AMI ID"
+  type        = string
+  default     = "ami-03f65b8614a860c29"
 }
 
 variable "user" {
@@ -34,14 +34,32 @@ variable "user" {
   default = "provisioner"
 }
 
-variable "instance_type" {
-  description = "The type of EC2 instance to launch"
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "main_instance_type" {
+  description = "The type of EC2 instance to main node"
   type = string
-  default = "t3.micro"
+  default = "t3.medium"
+}
+
+variable "storage_instance_type" {
+  description = "Instance type for storage nodes"
+  type        = string
+  default     = "t3.small" 
 }
 
 variable "instances_amount" {
   description = "The amount of instances to launch"
   type = number
   default = 3
+}
+
+variable "storage_volume_size" {
+  description = "Size in GB for each storage node EBS volume"
+  type        = number
+  default     = 100
 }
