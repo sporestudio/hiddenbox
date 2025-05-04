f588c7b8e114549473fce15a4a935b29e54ea0f6
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-05-04 15:14:40 +0200

    refactor: Normalize functions names and add desc
    Normalize functions names and add the descriptions of the differents
    fields in the documentation.

--- app/backend/src/lib/redis_service.py
+++ app/backend/src/lib/redis_service.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import redis
+from typing import List, Dict
 
 class RedisService:
     def __init__(self, url):
         self._redis = redis.Redis.from_url(url)
 
-    def save_metadata(self, file_uuid: str, user_id: str, key: str, created_at: str) -> None:
+    def store_metadata(self, file_uuid: str, user_id: str, key: str, created_at: str) -> None:
         """
         Save metadata of an encrypted file into Redis database.
 
         Args:
-            file_uuid (str): _description_
-            user_id (str): _description_
-            key (str): _description_
-            created_at (str): _description_
+            file_uuid (str): file's unique identifier
+            user_id (str): user's unique identifier
+            key (str): encryption key
+            created_at (str): timestamp of when the file was created
         """
         self._redis.hset(f"file:{file_uuid}", mapping={
             "user_id": user_id,
@@ -23,28 +24,28 @@ class RedisService:
             "created_at": created_at
         })
 
-    def save_fragments(self, file_uuid: str, fragments: list) -> None:
+    def store_fragments(self, file_uuid: str, fragments: list) -> None:
         """
         Save a list with the fragments' ids and their respective fragments.
 
         Args:
-            file_uuid (str): _description_
-            fragments (list): _description_
+            file_uuid (str): file's unique identifier
+            fragments (list): list of tuples with the fragment's index and the fragment itself
         """
         self._redis.rpush(f"file:{file_uuid}:fragments", *[str(idx) for idx, _ in fragments])
 
         for idx, frag in fragments:
             self._redis.set(f"fragment:{file_uuid}:{idx}", frag)
 
-    def load_metadata(self, file_uuid: str) -> dict:
+    def get_metadata(self, file_uuid: str) -> Dict:
         """
         Load file metadata from Redis database.
 
         Args:
-            file_uuid (str): _description_
+            file_uuid (str): file's unique identifier
 
         Returns:
-            dict: _description_
+            Dict: A dictionary with the file's metadata. The keys are the field names and the values are the field values.
         """
         data = self._redis.hgetall(f"file:{file_uuid}")
 
@@ -53,15 +54,15 @@ class RedisService:
 
         return {k.decode(): v.decode() for k,v in data.items()}
     
-    def load_fragments(self, file_uuid: str) -> list:
+    def get_fragments(self, file_uuid: str) -> List:
         """
         Get the stored fragments files from Redis database.
 
         Args:
-            file_uuid (str): _description_
+            file_uuid (str): file's unique identifier
 
         Returns:
-            list: _description_
+            List: A list of tuples with the fragment's index and the fragment itself.
         """
         idxs = self._redis.lrange(f"file:{file_uuid}:fragments", 0, -1)
         result = []
