ece35b345bc93e652927b421e71d52acbed8a0b6
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-05-04 12:37:49 +0200

    refactor: Correct few things on redis service
    - Fix blank spaces, and add blank space where was necessary
    - Document functions
    - Fix typo error in load fragments

--- app/backend/src/lib/redis_service.py
+++ app/backend/src/lib/redis_service.py
@@ -3,12 +3,20 @@
 
 import redis
 
-
 class RedisService:
     def __init__(self, url):
         self._redis = redis.Redis.from_url(url)
 
-    def save_metadata(self, file_uuid: str, user_id: str, key: str, created_at: str) -> None :
+    def save_metadata(self, file_uuid: str, user_id: str, key: str, created_at: str) -> None:
+        """
+        Save metadata of an encrypted file into Redis database.
+
+        Args:
+            file_uuid (str): _description_
+            user_id (str): _description_
+            key (str): _description_
+            created_at (str): _description_
+        """
         self._redis.hset(f"file:{file_uuid}", mapping={
             "user_id": user_id,
             "key": key,
@@ -16,21 +24,51 @@ class RedisService:
         })
 
     def save_fragments(self, file_uuid: str, fragments: list) -> None:
+        """
+        Save a list with the fragments' ids and their respective fragments.
+
+        Args:
+            file_uuid (str): _description_
+            fragments (list): _description_
+        """
         self._redis.rpush(f"file:{file_uuid}:fragments", *[str(idx) for idx, _ in fragments])
+
         for idx, frag in fragments:
             self._redis.set(f"fragment:{file_uuid}:{idx}", frag)
 
     def load_metadata(self, file_uuid: str) -> dict:
+        """
+        Load file metadata from Redis database.
+
+        Args:
+            file_uuid (str): _description_
+
+        Returns:
+            dict: _description_
+        """
         data = self._redis.hgetall(f"file:{file_uuid}")
+
         if not data:
             return {}
+
         return {k.decode(): v.decode() for k,v in data.items()}
     
     def load_fragments(self, file_uuid: str) -> list:
-        idxs = self._redis.lrange(f"file{file_uuid}:fragments", 0, -1)
+        """
+        Get the stored fragments files from Redis database.
+
+        Args:
+            file_uuid (str): _description_
+
+        Returns:
+            list: _description_
+        """
+        idxs = self._redis.lrange(f"file:{file_uuid}:fragments", 0, -1)
         result = []
+
         for binary_idx in idxs:
             idx = int(binary_idx.decode())
             frag = self._redis.get(f"fragment:{file_uuid}:{idx}")
             result.append(idx, frag)
+
         return result
