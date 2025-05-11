29ad4b8b994c5f070e82a5c5e46ed05c4b622681
jorgerodriguez-palisis <jorge.rodriguez@palisis.com>
2025-05-11 16:46:55 +0200

    refactor: modify main entrypoint
    Add authentication provider and browser router to app in entrypoint.

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,11 +1,16 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';      
-import HiddenBox from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HiddenBox />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
