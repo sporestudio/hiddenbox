324a1af9f6de98151b3cc784c10c0a4d484962cd
sporestudio <jrodcas@proton.me>
2025-05-25 19:35:35 +0200

    refactor: Set props as private and change key format
    - Set bucket and s3 property as private so anybody can use outside the
      object.
    - Change format key for a cleaner format.

--- src/api/services/s3_service.py
+++ src/api/services/s3_service.py
@@ -10,18 +10,17 @@ It uses the Boto3 library to interact with AWS S3.
 
 import boto3
 
-class s3Service:
+class S3:
     def __init__(self):
         """
         Initialize the S3 client and set the bucket name.
-        The bucket name is set to "main_storage" by default.
 
         Note: Ensure that the AWS credentials are set in the environment variables
         """
-        self.s3 = boto3.client("s3")
-        self.bucket = "main_storage"
+        self._s3 = boto3.client("s3")
+        self._bucket = "main_storage"
 
-    def store_fragment(self, file_uuid: str, fragment_index: int, data: bytes) -> None:
+    def store_fragment(self, user_id: str, file_uuid: str, fragment_index: int, data: bytes) -> None:
         """
         Store a file fragment in S3.
 
@@ -29,14 +28,11 @@ class s3Service:
             file_uuid (str): The UUID of the file.
             fragment_index (int): The index of the fragment.
             data (bytes): The data to be stored.
-
-        Returns:
-            None
         """
-        key = f"{file_uuid}/fragment_{fragment_index}"
-        self.s3.put_object(Bucket=self.bucket, Key=key, Body=data)
+        key = f"{user_id}/{file_uuid}/{fragment_index}.bin"
+        self._s3.put_object(Bucket=self.bucket, Key=key, Body=data)
 
-    def get_fragment(self, file_uuid: str, fragment_index: int) -> bytes:
+    def get_fragment(self, user_id: str, file_uuid: str, fragment_index: int) -> bytes:
         """
         Retrieve a file fragment from S3.
 
@@ -46,10 +42,11 @@ class s3Service:
 
         Returns:
             bytes: The data of the fragment.
+
         Raises:
             Exception: If the fragment is not found in S3.
         """
-        key = f"{file_uuid}/fragment_{fragment_index}"
-        object = self.s3.get_object(Bucket=self.bucket, Key=key)
+        key = f"{user_id}/{file_uuid}/{fragment_index}.bin"
+        obj = self._s3.get_object(Bucket=self._bucket, Key=key)
 
-        return object["Body"].read()
+        return obj["Body"].read()
