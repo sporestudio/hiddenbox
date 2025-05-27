3778ed7b1df1141b419edffffda3f0be8b89f19c
sporestudio <jrodcas@proton.me>
2025-05-27 21:14:48 +0200

    refactor: Handle errors in s3 service
    Implement custom exceptions in s3 service and handle errors with the
    bucket name.

--- src/api/services/s3_service.py
+++ src/api/services/s3_service.py
@@ -12,6 +12,8 @@ import os
 
 import boto3
 from dotenv import load_dotenv
+from .exceptions import BucketNotDefined, FragmentNotFound, S3OperationFailed
+from lib.datatypes import EncryptedFile, FileFragment
 
 load_dotenv()
 
@@ -23,9 +25,14 @@ class S3:
         Note: Ensure that the AWS credentials are set in the environment variables
         """
         self._s3 = boto3.client("s3")
-        self._bucket = os.getenv("S3_BUCKET_NAME")
+        _bucket = os.getenv("S3_BUCKET_NAME")
 
-    def store_fragment(self, user_id: str, file_uuid: str, fragment_index: int, data: bytes) -> None:
+        if _bucket is None:
+            raise BucketNotDefined()
+        
+        self._bucket = _bucket
+
+    def store_fragment(self, user_id: str, encrypted: EncryptedFile, fragment: FileFragment) -> None:
         """
         Store a file fragment in S3.
 
@@ -34,8 +41,16 @@ class S3:
             fragment_index (int): The index of the fragment.
             data (bytes): The data to be stored.
         """
-        key = f"{user_id}/{file_uuid}/{fragment_index}.bin"
-        self._s3.put_object(Bucket=self.bucket, Key=key, Body=data)
+        key = f"{user_id}/{encrypted.uuid}/{fragment.index}.bin"
+        print(f"Storing fragment at key: {key}")
+
+        try:
+            self._s3.put_object(Bucket=self._bucket, Key=key, Body=fragment.data)
+
+        except Exception as e:
+            print(f"Error uploading to S3: {e}")
+            print(f"Bucket: {self._bucket}, Key: {key}")
+            raise S3OperationFailed(operation="put_object") from e
 
     def get_fragment(self, user_id: str, file_uuid: str, fragment_index: int) -> bytes:
         """
@@ -51,7 +66,16 @@ class S3:
         Raises:
             Exception: If the fragment is not found in S3.
         """
-        key = f"{user_id}/{file_uuid}/{fragment_index}.bin"
-        obj = self._s3.get_object(Bucket=self._bucket, Key=key)
+        if not isinstance(fragment_index, int):
+            raise TypeError(f"Invalid fragment index: {fragment_index}")
 
-        return obj["Body"].read()
+        key = f"{user_id}/{file_uuid}/{fragment_index}.bin"
+        
+        try:
+            obj = self._s3.get_object(Bucket=self._bucket, Key=key)
+            return obj["Body"].read()
+        
+        except self._s3.exceptions.NoSuchKey:
+            raise FragmentNotFound(fragment_key=key)
+        except Exception as e:
+            raise S3OperationFailed(operation="get_object") from e
